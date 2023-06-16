e865e56a7b71ebac665ebf981e8a7b3b5aaf8c74
dener1974 <dener1974@icloud.com>
2023-06-16 19:15:07 +0200

    qweqweasd

--- conftest.py
+++ conftest.py
@@ -2,18 +2,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# chrome_options = Options()
-# chrome_options.add_argument("--headless")
-# driver = webdriver.Chrome(options=chrome_options)
-
 
 @pytest.fixture(scope='function')
-
 def driver():
     driver = webdriver.Chrome()
     driver.maximize_window()
     yield driver
     driver.quit()
+
+
 """
 Этот код представляет собой фикстуру Pytest, которая создает и настраивает экземпляр драйвера Chrome 
 для автоматизации тестирования веб-приложений. Фикстура имеет область видимости 'function', что означает, 

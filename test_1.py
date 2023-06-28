047caa8c76f56646b2f7e1ae72f95a2b0cd5460d
dener1974 <dener1974@icloud.com>
2023-06-28 18:00:55 +0200

    qweqwe

--- test_1.py
+++ test_1.py
@@ -3,7 +3,7 @@ import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options # для запуска браузера в безголовом режиме
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
